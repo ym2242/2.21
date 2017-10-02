bdbfe759d276d77f5be474e8bb1491a751dd6a02
Yi-Ming You <t106360241@ntut.org.tw>
2017-10-02 20:48:07 +0800

    HW

---
+++ source/Main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(void)
+{
+	printf("  *********\n  *       *\n  *       *\n  *       *\n  *       *\n  *       *\n  *       *\n  *       *\n  *********\n\n");
+	printf("     ***   \n   *     * \n  *       *\n  *       *\n  *       *\n  *       *\n  *       *\n   *     * \n     ***   \n\n");
+	printf("      *  \n     *** \n    *****\n      *  \n      *  \n      *  \n      *  \n      *  \n      *  \n\n");
+	printf("      *    \n     * *   \n    *   *\n   *     *\n  *       *\n   *     *\n    *   *\n     * *\n      *\n\n");
+		system("pause");
+		return 0;
+}
